d881c8d2a91f6aefb0ba7056b15864a2fe2513b3
Namanjain026 <namanjain9404@gmail.com>
2025-06-11 19:02:12 +0530

    add library page

--- app/(pages)/(afterAuth)/kindle-library/page.tsx
+++ app/(pages)/(afterAuth)/kindle-library/page.tsx
@@ -1,9 +1,116 @@
-import React from 'react'
+"use client";
+import Navbar from "@/components/Navbar";
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import styles from "./kindlelibrary.module.css";
+import { FaBookOpen } from "react-icons/fa";
+import { FaChevronRight, FaChevronDown } from "react-icons/fa6";
+import { MdBook } from "react-icons/md";
+import { useRouter } from "next/navigation";
+const apiurl = process.env.NEXT_PUBLIC_API_URL;
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  const router = useRouter();
+  const [show, setShow] = useState(false);
+  const [allBooks, setAllBooks] = useState<Book[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
-export default page
+  const getData = () => {
+    let temp = [
+      {
+        id: 1,
+        image: "https://picsum.photos/600/400",
+        title: "Book 1",
+        author: "Author 1",
+      },
+      {
+        id: 2,
+        image: "https://picsum.photos/600/400",
+        title: "Book 2",
+        author: "Author 2",
+      },
+      {
+        id: 3,
+        image: "https://picsum.photos/600/400",
+        title: "Book 3",
+        author: "Author 3",
+      },
+      {
+        id: 4,
+        image: "https://picsum.photos/600/400",
+        title: "Book 4",
+        author: "Author 4",
+      },
+    ];
+    setAllBooks(temp);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className={styles.main}>
+      <Navbar />
+      <div className={styles.row}>
+        <div className={styles.left}>
+          <div className={styles.menuMain}>
+            <FaBookOpen className={styles.bookicon} />
+            <p>Library</p>
+            {show ? (
+              <FaChevronDown
+                onClick={() => setShow(!show)}
+                className={styles.toRight}
+              />
+            ) : (
+              <FaChevronRight
+                onClick={() => setShow(!show)}
+                className={styles.toRight}
+              />
+            )}
+          </div>
+          {show && (
+            <div className={styles.menuItems}>
+              <span>All Titles</span>
+              <span>Books</span>
+              <span>Comics</span>
+              <span>Manga</span>
+              <span>Samples</span>
+            </div>
+          )}
+          <div className={styles.menuMain}>
+            <MdBook className={styles.bookicon2} />
+            <p>Notes & Highlights</p>
+          </div>
+        </div>
+        <div className={styles.right}>
+          <h1>Trending</h1>
+          <div className={styles.books}>
+            {allBooks.map((book) => (
+              <div
+                onClick={() => {
+                  router.push(`/book/${book.id}`);
+                }}
+                key={book._id}
+                className={styles.bookItem}
+              >
+                <img
+                  src={book.image}
+                  alt={book.title}
+                  className={styles.bookImage}
+                />
+                <div className={styles.bookDetails}>
+                  <h3 className={styles.bookTitle}>{book.title}</h3>
+                  <p className={styles.bookAuthor}>{book.author}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
